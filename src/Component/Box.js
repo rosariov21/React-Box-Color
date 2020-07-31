113a6d56dad78ebf2a914cd31b8b6b083fe7aa17
rosariov21 <rosariovelazquez47@yahoo.com>
2020-07-31 12:00:37 -0400

    first commit

---
+++ src/Component/Box.js
@@ -0,0 +1,27 @@
+import React from 'react';
+
+
+
+
+export default class Box extends React.Component{
+  constructor(props){
+    super(props);
+    this.state = { color:"blue" };
+    this.changeColor = this.changeColor.bind(this);
+  }
+  changeColor(){
+  let red = Math.floor(Math.random()*255)
+  let green = Math.floor(Math.random()*255)
+  let blue =  Math.floor(Math.random()*255)
+  console.log(this.state.color)
+  this.setState({color:`rgb(${red},${green},${blue})`})
+  }
+  render(){
+    return(
+      <div style={{backgroundColor: this.state.color}}>
+      <h1>Changed color</h1>
+      <button onClick={this.changeColor}>CHANGE MY COLOR</button>
+      </div>
+    )
+  }
+}
